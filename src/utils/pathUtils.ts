89928cff25c34b234334f45d4b5de5f19ec30b3c
Mateusz Jankowski <mateuszhg5@gmail.com>
2024-08-03 22:22:42 +0200

    add: path utilities for handling directory traversal

---
+++ src/utils/pathUtils.ts
@@ -0,0 +1,24 @@
+// src/utils/pathUtils.ts
+import * as fs from 'fs';
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+export async function getComponentFolders(basePath: string): Promise<vscode.QuickPickItem[]> {
+	const folders: vscode.QuickPickItem[] = [];
+
+	function traverseDirectory(currentPath: string) {
+		const items = fs.readdirSync(currentPath);
+
+		for (const item of items) {
+			const itemPath = path.join(currentPath, item);
+			if (fs.statSync(itemPath).isDirectory()) {
+				folders.push({ label: path.relative(basePath, itemPath) });
+				traverseDirectory(itemPath);
+			}
+		}
+	}
+
+	traverseDirectory(basePath);
+
+	return folders;
+}
